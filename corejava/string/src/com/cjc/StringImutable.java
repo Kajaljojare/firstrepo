0af8e4c1189d36749f04b42ca03680cf0e7bce67
Kajaljojare <kajaljojare7@gmail.com>
2019-07-12 18:29:43 +0530

    save changes after modify

---
+++ corejava/string/src/com/cjc/StringImutable.java
@@ -0,0 +1,16 @@
+package com.cjc;
+import java.lang.*;
+
+public class StringImutable
+{
+	public static void main(StringImutable[] args)
+	{
+		String s=new String ("dfs");
+		s.concat("classs");
+		System.out.println(s);
+	}
+
+	
+	
+
+}
